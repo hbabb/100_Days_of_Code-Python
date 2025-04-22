abede808862ef2738b9664f86d37d478a1080af5
heath <hbabb@heath-babb.com>
2025-04-21 20:54:39 -0400

    Day 1 completed

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Hello " + input("What is your name?") + "!")
+print("Welcome to the Band Name Generator")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("You band name is: " + city + " " + pet)
