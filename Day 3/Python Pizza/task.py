d3b7798aa0cdf53189728614ab434662abc023a5
heath <hbabb@heath-babb.com>
2025-04-24 21:15:59 -0400

    Day 3 In Progress
    Working through Day 3 projects and leaving off at Logical Operators

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,36 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+# todo: work out how much they need to pay based on their size choice.
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("You have entered an invalid size pizza")
+
+# todo: work out how much to add to their bill based on their pepperoni choice.
+# Add pepperoni for small pizza: +$2
+# Add pepperoni for medium or large pizza: +$3
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+# todo: work out their final amount based on whether if they want extra cheese.
+# Add extra chees for any size pizza: +$1
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
